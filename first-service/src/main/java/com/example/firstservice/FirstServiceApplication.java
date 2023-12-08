58fa3c1173811a301af622085c5c7bf1c171a666
honeydanji <tjdwls3680@naver.com>
2023-12-08 16:36:02 +0900

    # First Service : 의존성 수정 및 Controller 추가

--- first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
@@ -1,13 +1,17 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FirstServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstServiceApplication.class, args);
 	}
-
 }

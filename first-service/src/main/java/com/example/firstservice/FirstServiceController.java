ec456388658b555b0d876d469fb6cebbe2b02ce9
honeydanji <tjdwls3680@naver.com>
2023-12-19 23:05:15 +0900

    # First Service : Controller Http Request Port Confirm
    1. Console Log Confirm
    2. Http Response Confirm
    
    // TIP
    - 환경변수 port 를 0번으로 설정했기 때문에 랜덤 port 번호가 나옴.
    - Environment environment 를 이용하면 해당 프로젝트에 설정한 yml 파일 속성들을 가져올 수 있음 + @Value 어노테이션을 활용해도 무관

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,25 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment environment; // 환경변수(application.yml 설정 요소)
+
+    @Autowired
+    public FirstServiceController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,8 +33,13 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        // console 에서 log 확인
+        log.info("Server port={}", request.getServerPort());
+
+        // http 요청에 의한 응답으로 확인
+        return String.format("Hi, there. This is a message from First Service on PORT %s",
+                environment.getProperty("local.server.port"));
     }
     
 }

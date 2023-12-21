3919345df8ecf9db4ebb9a7f1ab18e8d427cd461
honeydanji <tjdwls3680@naver.com>
2023-12-21 16:53:34 +0900

    # FEAT(UserService) : Add dependency inside constructor

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,15 +6,24 @@ import com.example.userservice.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
+    UserRepository userRepository;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        // 해당 객체에 종속성을 주입 하기 위해서는 해당 필드들이 bean 에 등록이 된 상태여야 한다.
+        // app 이 실행되는 동시에 지정한 객체들은 자동으로 bean 에 등록된다.
+        // BCryptPasswordEncoder 같은 경우는 "UserServiceApplication"에서 @Bean 을 이용해 등록하였다.
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -23,7 +32,7 @@ public class UserServiceImpl implements UserService{
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // mapper 환경 설정
         UserEntity userEntity = mapper.map(userDto, UserEntity.class); // userDto 를 UserEntity.class 로 변환
-        userEntity.setEncryptedPwd("encrypted_password");
+        userEntity.setEncryptedPwd(bCryptPasswordEncoder.encode(userDto.getPwd()));
 
         userRepository.save(userEntity);
 

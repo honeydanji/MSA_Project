c8a6561866bd0ef5238667f0d55d091cdd87c194
honeydanji <tjdwls3680@naver.com>
2023-12-29 15:06:17 +0900

    FEAT(OrderService) : Add Dto, ResponseObject

---
+++ order-service/src/main/java/com/orderservice/vo/ResponseOrder.java
@@ -0,0 +1,18 @@
+package com.orderservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL) // json 을 반환할 때 null 값은 반환하지 않는다.
+public class ResponseOrder {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+    private Date createAt;
+
+    private String orderId;
+}

851e2dab51f850d170bf36ec918a7274d35ecf1f
honeydanji <tjdwls3680@naver.com>
2023-12-29 14:59:13 +0900

    FEAT(OrderService) : Add Jpa
    // Entity
    - Serializable 상속
    
    // Repository
    - CRUDRepository 상속

---
+++ order-service/src/main/java/com/orderservice/jpa/OrderEntity.java
@@ -0,0 +1,35 @@
+package com.orderservice.jpa;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class OrderEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 고유식별자
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private Integer qty;
+    @Column(nullable = false)
+    private Integer unitPrice;
+    @Column(nullable = false)
+    private Integer totalPrice;
+
+    @Column(nullable = false)
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP") // 기본값을 현재시간
+    private Date createdAt;
+}

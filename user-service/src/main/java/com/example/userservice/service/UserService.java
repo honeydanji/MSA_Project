e2a4324d12780341479e5757f782dd554392c76c
honeydanji <tjdwls3680@naver.com>
2024-01-04 16:57:02 +0900

    # FEAT(UserService) : Add UserService method

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -8,4 +8,5 @@ public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
     UserDto getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll(); // Iterable : List 와 비슷한 interface 인데 객체가 반복될 경우 사용하는 interface.
+    UserDto getUserDetailsByEmail(String userName);
 }
